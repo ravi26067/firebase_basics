1100e492d8d01c3de3a047f4185eafb2a33d5648
ravi26067 <ravi26067@gmail.com>
2020-07-07 20:31:20 +0530

    Firebase basic project

---
+++ app.js
@@ -0,0 +1,64 @@
+const cafeList = document.querySelector('#cafe-list')
+const form = document.querySelector('#add-cafe-form')
+
+//create element and render cafe
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+    cross.textContent = 'x';
+
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(cross);
+
+    cafeList.appendChild(li);
+
+    //deleting data
+    cross.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+}
+
+// Where  statement
+//db.collection('cafes').where('city','>','M').get().then((snapshot) => {
+
+
+//get the reference of of collection
+// db.collection('cafes').orderBy('city').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         //console.log(doc.data())
+//         renderCafe(doc);
+//     });
+// })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+})
+
+// real time listner
+
+db.collection('cafes').orderBy('name').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == 'added')
+            renderCafe(change.doc);
+        else if(change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li); 
+        }
+    });
+})
